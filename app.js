e34b586b978b1c8ac116ff074cee0d14b9448454
Margo Lanier <lanier.margo@gmail.com>
2016-12-29 15:25:59 -0500

    mustache ul template works with user input

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-words = ['red', 'orange', 'green', 'black', 'blue'];
+let possible = ['red', 'orange', 'green', 'black', 'blue'];
+let filtered = [];
 
 function init() {
-	window.addEventListener('input', function() {
-		console.log('key typed');
+	console.log(possible);
+	
+	let searchbox = document.querySelector('#searchbox');
+	
+	// On user input
+	searchbox.addEventListener('input', function(e) {
+		
+		query = e.target.value;
+		
+		// Check possible options for matches
+		for (let i=0; i<possible.length; i++) {
+			
+			if (possible[i].includes(query)) {
+				filtered.push(possible[i]);
+			}
+		}
+		
+		// Display updated list
+		let options = document.querySelector('#options');
+		options.innerHTML = Mustache.render(
+			'<ul>{{#.}}<li>{{.}}</li>{{/.}}</ul>',
+			filtered
+		)
+		
+		
 	});
 	
 }
